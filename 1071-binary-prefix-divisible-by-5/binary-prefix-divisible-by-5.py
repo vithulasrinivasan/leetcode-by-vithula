690d7108ccd16801578dc7f0211ec4e9af56680e
VITHULA S <115547879+vithulasrinivasan@users.noreply.github.com>
2025-11-24 14:35:48 +0530

    Time: 128 ms (46.96%) | Memory: 13.7 MB (66.09%) - LeetSync

--- 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.py
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.py
@@ -5,13 +5,25 @@ class Solution(object):
         :rtype: List[bool]
         """
 
-        n=len(nums)
-        ans=[]
-        prefix=0
-        for i in range(n):
-            prefix += 2**(n-i) * nums[i]
-            if prefix % 5 ==0:
-                ans.append(True)
-            else:
-                ans.append(False)
+        #shifting the digits left == multiplying it by 2
+        # if it is divisible by 5 , reinitialize it to 0
+
+        ans =[]
+        curr =0
+        for bit in nums:
+            curr = (curr<<1) + bit
+            ans.append(curr%5==0)
         return ans
+
+
+        # brute force
+        # n=len(nums)
+        # ans=[]
+        # prefix=0
+        # for i in range(n):
+        #     prefix += 2**(n-i) * nums[i]
+        #     if prefix % 5 ==0:
+        #         ans.append(True)
+        #     else:
+        #         ans.append(False)
+        # return ans
