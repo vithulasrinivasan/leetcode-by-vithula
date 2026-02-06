48cc58a59eee9ecc0511e2026527667bcf3cff18
VITHULA S <115547879+vithulasrinivasan@users.noreply.github.com>
2026-02-06 20:25:19 +0530

    Time: 75 ms (98.56%) | Memory: 34.6 MB (28.52%) - LeetSync

--- 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.py
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.py
@@ -1,14 +1,19 @@
 class Solution:
     def minRemoval(self, nums: List[int], k: int) -> int:
         nums.sort()
-        i=0
-        count = 0
+        a = 0
+        for c in nums:
+            a += c > nums[a] * k
+        return a
+        # nums.sort()
+        # i=0
+        # count = 0
 
-        for j in range(len(nums)):
-            while nums[j] > nums[i] * k:
-                i+=1
+        # for j in range(len(nums)):
+        #     while nums[j] > nums[i] * k:
+        #         i+=1
 
             
-            count = max(count, j-i+1)
+        #     count = max(count, j-i+1)
     
-        return len(nums) - count
+        # return len(nums) - count
