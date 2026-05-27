383d18fa27b582f53a37ea11f076d880a157ed33
VITHULA S <115547879+vithulasrinivasan@users.noreply.github.com>
2026-05-27 10:55:32 +0530

    Time: 42 ms (100.00%) | Memory: 21.6 MB (28.82%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.py
@@ -1,20 +1,31 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        first={}
-        last={}
 
-        for index,c in enumerate(word):
-            if c.isupper():
-                c=c.lower()
-                if c not in first:
-                    first[c]=index
-            else:
-                last[c]=index
+        ans=0
 
-        count=0
+        for ch, CH in zip(ascii_lowercase, ascii_uppercase):
 
-        for c in string.ascii_lowercase:
-            if c in first and c in last and first[c] > last[c]:
-                count+=1
+            if ch not in word or CH not in word: continue
+  
+            ans+= word.rfind(ch) < word.find(CH)
+
+        return ans
+
+        # first={}
+        # last={}
+
+        # for index,c in enumerate(word):
+        #     if c.isupper():
+        #         c=c.lower()
+        #         if c not in first:
+        #             first[c]=index
+        #     else:
+        #         last[c]=index
+
+        # count=0
+
+        # for c in string.ascii_lowercase:
+        #     if c in first and c in last and first[c] > last[c]:
+        #         count+=1
         
-        return count
+        # return count
